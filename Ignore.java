a54ad72626581a2692e7b3e8556cf116a6820583
jenkinshome <jenkinsgithub409@gmail.com>
2022-04-01 08:28:11 +0530

    Ignoring Both .class and .java Files

--- Ignore.java
+++
@@ -1,7 +0,0 @@
-class Ignore {
- public static void main(String args[]) {
-  System.out.println("Ignoring Both Files");
-  System.out.println("First Ignoring .class file");
-  System.out.println("Second Ignoring Both .class and .java Files");
-  }
-}
